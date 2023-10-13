86b1f3198c8d498205e161d8c9930df355522f18
13910014922 <18239256@qq.com>
2023-10-13 22:47:30 +0800

    创建User页面框架

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+'use client'
 
-import {signOut} from "next-auth/react";
+import EmptyState from '../components/EmptyState';
 
 const Users = () => {
-    return (
-        <button onClick={()=> signOut()}>
-            注销
-        </button>
-    );
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+   );
 }
-
+ 
 export default Users;
