d0a885b108980b829cac0532f983dde8db2e8b6d
18239256 <18239256@qq.com>
2023-10-29 00:10:01 +0800

    修改消息气泡的边缘弧度

--- app/robots/[conversationId]/components/MessageBox.tsx
+++ app/robots/[conversationId]/components/MessageBox.tsx
@@ -34,7 +34,7 @@ const MessageBox: React.FC<MessageBoxProps> = ({
     const message = clsx(
         'text-sm w-fit overflow-hidden',
         isOwn ? 'bg-sky-500 text-white' : 'bg-gray-100',
-        data.image ? 'rounded-md p-0' : 'rounded-full py-2 px-3'
+        data.image ? 'rounded-md p-0' : 'rounded-lg py-2 px-3'
     );
 
     return (
