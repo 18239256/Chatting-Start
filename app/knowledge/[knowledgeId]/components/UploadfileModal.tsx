6139274958e88166975407542627de8e9a33c1ea
18239256 <18239256@qq.com>
2023-11-16 21:42:13 +0800

    完成多文件上传基础能力

--- app/knowledge/[knowledgeId]/components/UploadfileModal.tsx
+++ app/knowledge/[knowledgeId]/components/UploadfileModal.tsx
@@ -7,6 +7,8 @@ import { Button, Input } from "@nextui-org/react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Knowledge } from "@prisma/client";
+import { File } from "buffer";
+import { stringify } from "querystring";
 
 interface UploadfileModalProps {
     isOpen: boolean;
@@ -21,13 +23,19 @@ const UploadfileModal: React.FC<UploadfileModalProps> = ({
 }) => {
 
     const [isLoading, setIsLoading] = useState(false);
+    const [uploadFiles, setUploadFiles] = useState(['']);
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         console.log('data', { ...data });
         const form = new FormData();
         form.append("knowledge_base_name", knowledge.realName);
-        form.append("files",data.files[0]);
-        console.log('form', { ...form });
+        let fileLength = data.files.length;
+        let i = 0;
+        while (i < fileLength) {
+            let file = data.files[i];
+            form.append("files", file);
+            i++;
+        }
         axios.post('http://region-31.seetacloud.com:38744/api/knowledge_base/upload_docs', form)
         .then((res)=>{
             console.log('res', res);
@@ -49,6 +57,19 @@ const UploadfileModal: React.FC<UploadfileModalProps> = ({
         defaultValues: {
         }
     });
+
+    const fileSelected = (data: any) => {
+        let files = data.target.files;
+        let fileLength = files.length;
+        let i = 0;
+        let fileNames = [];
+        while (i < fileLength) {
+            let file = files[i];
+            fileNames.push(file.name);
+            i++;
+        }
+        setUploadFiles([...fileNames]);
+    }
 
     return (
         <Modal
@@ -73,7 +94,13 @@ const UploadfileModal: React.FC<UploadfileModalProps> = ({
                         </p>
                     </div>
                     <div>
-                        <input type="file" {...register('files')}></input>
+                        <label htmlFor="files" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                            <div className=" border-2 border-red-400 flex justify-center">
+                            <span>Upload a file</span>
+                            </div>
+                        </label>
+                        <p><input id="files" type="file" {...register('files')} onChange={fileSelected} className="sr-only" multiple></input></p>
+                        {uploadFiles.map((f)=>(<div>{f}</div>))}
                     </div>
                     <div>
                         <Button disabled={isLoading} type="submit" color="primary" variant="flat">
