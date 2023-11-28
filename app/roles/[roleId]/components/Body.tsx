a26bef59e3f6568c8bed67ae6f924e4618a3395b
18239256 <18239256@qq.com>
2023-11-28 13:15:11 +0800

    完成样式

--- app/roles/[roleId]/components/Body.tsx
+++ app/roles/[roleId]/components/Body.tsx
@@ -1,20 +1,42 @@
 'use client';
 
 import { Role, User } from "@prisma/client";
-import { Listbox, ListboxItem, Chip, ScrollShadow, Selection } from "@nextui-org/react";
+import { Listbox, ListboxItem, Chip, ScrollShadow, Selection, Button } from "@nextui-org/react";
 import Avatar from "@/app/components/Avatar";
 import { ListboxWrapper } from "./ListboxWrapper";
-import { useMemo, useState } from "react";
+import { Key, useEffect, useMemo, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 interface BodyProps {
     role: Role;
     users: User[];
 }
 
+function arrayEqual(a:Key[], b:Key[]) {
+    //先将数组排序
+    a = a.sort();
+    b = b.sort();
+    //判断数组长度是否相等，若不相等返回false
+    if (a.length != b.length) 
+    return false;
+    //逐个比较数组元素
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) 
+        return false;
+    }
+    return true;
+}
 
 const Body: React.FC<BodyProps> = ({ role, users = [] }) => {
     const [values, setValues] = useState<Selection>(new Set(role.assignIds));
     const arrayValues = Array.from(values);
+    const [dirty, setDirty] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setDirty(!arrayEqual(role.assignIds, Array.from(values)));
+    }, [role.assignIds, values]);
 
     const topContent = useMemo(() => {
         if (!arrayValues.length) {
@@ -32,11 +54,38 @@ const Body: React.FC<BodyProps> = ({ role, users = [] }) => {
         );
     }, [arrayValues.length]);
 
+    const saveModify = () =>{
+        setIsLoading(true);
+        const param={
+            roleId: role.id,
+            assignIds: values
+        }
+        axios.post('/api/addUsers', param)
+        .then((callback) => {
+          if (callback?.status !== 200) {
+            toast.error('保存失败');
+          }
+  
+          if (callback?.status == 200) {
+            toast.success('保存成功');
+          }
+        })
+        .catch(() => toast.error('出错了!'))
+        .finally(() => setIsLoading(false));
+    };
+
     return (
         <>
             <div className="flex-1 overflow-y-auto px-4 py-4">
-                <div className="flex-1 justify-between overflow-y-auto py-4">
-                    用户管理
+                <div className="flex justify-between overflow-y-auto py-4">
+                    <div className="py-2">用户管理</div>
+                    {dirty && (
+                        <div>
+                            <Button color="primary" className="bg-sky-500 hover:bg-sky-600" onClick={saveModify}>
+                                保存修改
+                            </Button>
+                        </div>
+                    )}
                 </div>
                 <ListboxWrapper>
                     <Listbox
