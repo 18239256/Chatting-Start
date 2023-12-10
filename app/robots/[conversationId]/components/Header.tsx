c61252d68f7ca2f4ad4bcee41dc04f22a80666cd
18239256 <18239256@qq.com>
2023-12-10 11:04:08 +0800

    删除调试log

--- app/robots/[conversationId]/components/Header.tsx
+++ app/robots/[conversationId]/components/Header.tsx
@@ -61,7 +61,6 @@ const Header: React.FC<HeaderProps> = ({ conversation, masks}) => {
   };
 
   const disconnectShareRobot = () =>{
-    console.log('robotUser.robot?.id', robotUser.robot?.id);
     axios.post('/api/robot/robotconsumecut', { robotId: robotUser.robot?.id })
       .then()
       .catch(() => toast.error('出错了!'))
